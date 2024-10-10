6f192eb074eb37f9c4a39f796f1d61db5e9bef60
kishlakhub <gsubbotin.2019@gmail.com>
2024-10-10 16:18:00 +0500

    Созданы модели афиши и кинотеатра. Субботин

---
+++ Cinemas_Subbotin/Classes/EqualityComparer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cinemas_Subbotin.Classes
+{
+    public class EqualityComparer
+    {
+        public bool Equals(KInoteatrContext k1, KInoteatrContext k2)
+        {
+            if (k1 == null || k2 == null)
+                return false;
+            else if (k1.Id == k2.Id &&
+                k1.Name == k2.Name &&
+                k1.Count == k2.Count &&
+                k1.CountZal == k2.CountZal)
+                return true;
+            else
+                return false;
+        }
+    }
+}
---
+++ Cinemas_Subbotin/Models/Kinoteatr.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cinemas_Subbotin.Models
+{
+    public class Kinoteatr
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int CountZal { get; set; }
+        public int Count { get; set; }
+        public Kinoteatr(int Id, string Name, int CountZal, int Count)
+        {
+            this.Id = Id;
+            this.Name = Name;
+            this.CountZal = CountZal;
+            this.Count = Count;
+        }
+    }
+}
